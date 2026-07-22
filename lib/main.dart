f292a184864a23f61f72ce91d8bfd331a0f24627
Santhosh VS <santhosh02vs@gmail.com>
2026-07-22 15:09:49 +0530

    feat: implement initial ExpenseApp and ExpenseHome widgets with basic MaterialApp structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const ExpenseApp() );
+  runApp(const ExpenseApp());
 }
 
 class ExpenseApp extends StatelessWidget {
   // ExpenseApp({key? key}):super(key: key);
- const ExpenseApp({super.key})
-
+  const ExpenseApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }}
+    return MaterialApp(home: Text("Hello Sandy"));
+  }
+}
+
+class ExpenseHome extends StatelessWidget {
+  const ExpenseHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text("Hello Sandy"));
+  }
+}
